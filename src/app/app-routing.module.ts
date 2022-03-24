2e083bf76883a97f2adfbab5ca095c774e52e2b7
Igovi <igovi9910@gmail.com>
2022-03-24 18:19:47 -0300

    add:creating characters page + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { CharactersComponent } from './pages/characters/characters.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'characters',
+    component: CharactersComponent
+  },
+  {
+    path: '',
+    redirectTo: 'characters',
+    pathMatch: 'full'
+  },
+  {
+    path: 'characters',
+    redirectTo: 'characters',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
