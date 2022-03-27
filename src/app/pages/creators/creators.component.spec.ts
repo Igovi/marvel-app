f5bf8c9e246c94a77511a338fbbc4ac4a67f35fd
Igovi <igovi9910@gmail.com>
2022-03-26 22:06:51 -0300

    add: creators page unit test

--- src/app/pages/creators/creators.component.spec.ts
+++ src/app/pages/creators/creators.component.spec.ts
@@ -2,12 +2,15 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreatorsComponent } from './creators.component';
 
+import { HttpClientTestingModule} from '@angular/common/http/testing';
+
 describe('CreatorsComponent', () => {
   let component: CreatorsComponent;
   let fixture: ComponentFixture<CreatorsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports : [HttpClientTestingModule],
       declarations: [ CreatorsComponent ]
     })
     .compileComponents();
@@ -21,5 +24,20 @@ describe('CreatorsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call getCreators function', () => {
+    spyOn(component, 'getCreators').and.callThrough();
+    component.getCreators();
+    expect(component.getCreators).toHaveBeenCalled();
+  });
+  it('should call getThumbnail function', () => {
+    spyOn(component, 'getThumbnail').and.callThrough();
+    component.getThumbnail();
+    expect(component.getThumbnail).toHaveBeenCalled();
+  });
+  it('should call initEmitSubscritions function', () => {
+    spyOn(component, 'initEmitSubscritions').and.callThrough();
+    component.initEmitSubscritions();
+    expect(component.initEmitSubscritions).toHaveBeenCalled();
   });
 });
