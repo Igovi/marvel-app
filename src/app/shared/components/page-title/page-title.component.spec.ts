29c88a209da163bef518dc6750f391c0d0765b57
Igovi <igovi9910@gmail.com>
2022-03-26 22:07:16 -0300

    add: page-title component unit test

--- src/app/shared/components/page-title/page-title.component.spec.ts
+++ src/app/shared/components/page-title/page-title.component.spec.ts
@@ -2,12 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageTitleComponent } from './page-title.component';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
+
 describe('PageTitleComponent', () => {
   let component: PageTitleComponent;
   let fixture: ComponentFixture<PageTitleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports : [RouterTestingModule],
       declarations: [ PageTitleComponent ]
     })
     .compileComponents();
@@ -21,5 +25,10 @@ describe('PageTitleComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should call ngOnInit function', () => {
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
   });
 });
